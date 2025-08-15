a3a708a60ae3c7f6fb04e21113b7c9d65b31cb3a
isabellyccamargo <camargoisa114@gmail.com>
2025-08-14 22:37:22 -0300

    Subindo a tela de login em que foi iniciada o frontend

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -5,18 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="./style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ancizar+Serif:ital,wght@0,300..900;1,300..900&family=Bitter:ital,wght@0,100..900;1,100..900&family=Caudex:ital,wght@0,400;0,700;1,400;1,700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Marcellus&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Padauk:wght@400;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
     <div class="login-container">
-        <div class="logo"></div>
+        <div class="logo">
+            <img src="../../../fotos/logo.jpg" alt="Logo da Empresa" class="logo">
+        </div>
         <form class="login-form" action="login.php" method="POST">
             <div class="input-group">
                 <label for="email">email</label>
                 <input type="email" id="email" name="email" placeholder=" ">
             </div>
             <div class="input-group">
+                <div class="input-group">
                 <label for="password">senha</label>
                 <input type="password" id="password" name="password" placeholder=" ">
+                <div class="show-password-container">
+                    <input type="checkbox" id="show-password">
+                    <label for="show-password" class="show-password-label">Mostrar senha</label>
+                </div>
+              </div>
             </div>
             <button type="submit" class="access-button">acessar</button>
         </form>
@@ -26,3 +38,18 @@
     </div>
 </body>
 </html>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+    const passwordInput = document.getElementById('password');
+    const showPasswordCheckbox = document.getElementById('show-password');
+
+    showPasswordCheckbox.addEventListener('change', function() {
+        if (showPasswordCheckbox.checked) {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+        }
+    });
+});
+</script>
